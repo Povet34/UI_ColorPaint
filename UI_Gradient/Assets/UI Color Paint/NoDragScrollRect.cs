a0a7b2bdafdff7f27096d2ba105c52c1580ca3ca
Povet34 <70003696+Povet34@users.noreply.github.com>
2024-12-10 10:12:22 +0900

    NoDragScrollRect 구현

---
+++ UI_Gradient/Assets/UI Color Paint/NoDragScrollRect.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class NoDragScrollRect : ScrollRect
+{
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+    }
+}
