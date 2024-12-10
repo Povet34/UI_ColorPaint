d079f51ce294db0d119c65d5a7accee752fb6755
Povet34 <70003696+Povet34@users.noreply.github.com>
2024-12-11 08:20:45 +0900

    Timer에 맞게 서서히 StartColor가 이동

--- UI_Gradient/Assets/UI_Gradient/GradientSegment.cs
+++ UI_Gradient/Assets/UI_Gradient/GradientSegment.cs
@@ -7,7 +7,6 @@ public class GradientSegment : MonoBehaviour
     [SerializeField] RawImage endImage;
 
     [SerializeField] RawImage gradientArea;
-    [SerializeField] RawImage copyGradientArea;
 
     [SerializeField] float changeTime;
 
@@ -33,23 +32,17 @@ public class GradientSegment : MonoBehaviour
         Vector2 dir = end - start;
 
         gradientArea.rectTransform.anchoredPosition = start;
-        copyGradientArea.rectTransform.anchoredPosition = start;
 
         gradientArea.rectTransform.sizeDelta = new Vector2(dir.magnitude, startImage.rectTransform.sizeDelta.y);
-        copyGradientArea.rectTransform.sizeDelta = new Vector2(dir.magnitude, startImage.rectTransform.sizeDelta.y * 0.7f);
 
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         gradientArea.rectTransform.rotation = Quaternion.Euler(0, 0, angle);
-        copyGradientArea.rectTransform.rotation = Quaternion.Euler(0, 0, angle);
 
         gradientArea.material.SetColor("_StartColor", startImage.color);
-        copyGradientArea.material.SetColor("_StartColor", startImage.color);
 
         gradientArea.material.SetColor("_EndColor", endImage.color);
-        copyGradientArea.material.SetColor("_EndColor", endImage.color);
 
         gradientArea.material.SetColor("_Emission", Color.white);
-        copyGradientArea.material.SetColor("_Emission", new Color(0, 0, 0, 0));
 
         timer += Time.deltaTime;
     }
