e42debfecfe9e377eae24a7a2b361d27a83b748c
Povet34 <70003696+Povet34@users.noreply.github.com>
2024-12-11 11:04:38 +0900

    eroding Light 구현

--- UI_Gradient/Assets/UI_Gradient/ErodingGradientSegment.cs
+++ UI_Gradient/Assets/UI_Gradient/ErodingGradientSegment.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GradientSegment : MonoBehaviour
+public class ErodingGradientSegment : MonoBehaviour
 {
     [SerializeField] RawImage startImage;
     [SerializeField] RawImage endImage;
     [SerializeField] RawImage gradientArea;
 
     [SerializeField] float changeTime;
-    [SerializeField] float flowSpeed = 5;
+    [SerializeField] float erodingTime = 5;
 
     float timer;
+    float erodingTimer;
 
     private void Update()
     {
@@ -19,11 +20,12 @@ public class GradientSegment : MonoBehaviour
 
     private void UpdateGrdient()
     {
-        if(timer > changeTime)
+        if (timer > changeTime)
         {
             startImage.color = new Color(Random.value, Random.value, Random.value);
             endImage.color = new Color(Random.value, Random.value, Random.value);
             timer = 0;
+            erodingTimer = 0;
         }
 
         Vector2 start = startImage.rectTransform.anchoredPosition;
@@ -40,8 +42,18 @@ public class GradientSegment : MonoBehaviour
 
         gradientArea.material.SetColor("_StartColor", startImage.color);
         gradientArea.material.SetColor("_EndColor", endImage.color);
-        gradientArea.material.SetFloat("_FlowSpeed", flowSpeed);
+
+        float erodingUV = CalcErodingUV();
+        gradientArea.material.SetFloat("_ErodingTime", erodingUV);
 
         timer += Time.deltaTime;
+        erodingTimer += Time.deltaTime;
+    }
+
+    private float CalcErodingUV()
+    {
+        float uv = Mathf.Clamp01(erodingTimer / erodingTime);
+        Debug.Log(uv);
+        return uv;
     }
 }
